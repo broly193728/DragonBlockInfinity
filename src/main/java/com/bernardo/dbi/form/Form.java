73920e8d85637829aa3dd64acc4bfdcf80931c73
broly193728 <bernardopfgd@gmail.com>
2025-12-27 20:11:11 +0000

    Correção no enum Form: adicionado construtor e campo id para suportar parâmetro BaseForm.ID

--- src/main/java/com/bernardo/dbi/form/Form.java
+++ src/main/java/com/bernardo/dbi/form/Form.java
@@ -1,5 +1,15 @@
 package com.bernardo.dbi.form;
 
 public enum Form {
-    Base(BaseForm.ID)
+    Base(BaseForm.ID);
+
+    private final String id;
+
+    Form(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
